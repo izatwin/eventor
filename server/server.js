e9fa5206dc8f1becce7aa347a7f2fab8763bfe9f
izatwin <izatwin@cheezers.org>
2024-10-06 15:21:09 -0400

    simplify server.js

--- server/server.js
+++ server/server.js
@@ -1,24 +1,16 @@
 const createError = require('http-errors');
 const express = require('express');
-const bodyParser = require("body-parser");
-// const cors = require("cors");
+const cors = require("cors");
+require('dotenv').config({ path: '../.env' });
 
 const mongoose = require("mongoose")
-// Set `strictQuery: false` to globally opt into filtering by properties that aren't in the schema
-// Included because it removes preparatory warnings for Mongoose 7.
-// See: https://mongoosejs.com/docs/migrating_to_6.html#strictquery-is-removed-and-replaced-by-strict
-mongoose.set("strictQuery", false)
-// Define the database URL to connect to
-const mongoDB = "mongodb://127.0.0.1/my_database"
 
-// Wait for database to connect, logging an error if there is a problem
-main().catch((err) => console.log(err));
-async function main() {
-  await mongoose.connect(mongoDB)
-}
+// MongoDB connection
+mongoose.connect(process.env.DB_CONNECTION)
+  .then(() => console.log('Connected to database'))
+  .catch((error) => console.log(error));
 
 
-const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
 
@@ -27,20 +19,13 @@ const postRouter = require('./routes/posts')
 
 const app = express();
 
-// var corsOptions = {
-//     origin: "http://localhost:8081"
-//   };
 
 // parse requests of content-type - application/json
-app.use(bodyParser.json());
-
-// parse requests of content-type - application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(cors());
 
 
 app.use(logger('dev'));
-app.use(express.json());
-// app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 
 app.use('/', indexRouter);
@@ -62,7 +47,7 @@ app.use(function(err, req, res, next) {
   res.send(err);
 });
 
-const PORT = 3000;
+const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`)
 })
