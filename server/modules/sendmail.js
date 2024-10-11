410c3bc424f51f0b9a019db510984a9e3816bb7c
Vmena9 <vmenapace@icloud.com>
2024-10-10 23:44:36 -0400

    Account creation confirmation emails added

--- server/modules/sendmail.js
+++ server/modules/sendmail.js
@@ -126,7 +126,7 @@ app.listen(PORT, () => {
 async function sendVerificationEmail(recipientEmail, verificationCode) {
   const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
   let subject = "Here's your verification code " + verificationCode;
-  let body = `Enter the 6-digit code below to verify your identity and gain access to your Eventor account.\n\n${verificationCode}`;
+  let body = `Enter the 6-digit code below to verify your identity and gain access to your Eventor account.\n\n${verificationCode}\n\nThis code will expire in 10 minutes.`;
 
   const raw = makeBody(recipientEmail, 'me', subject, body);
 
@@ -142,11 +142,31 @@ async function sendVerificationEmail(recipientEmail, verificationCode) {
   }
 }
 
+async function sendConfirmationEmail(recipientEmail, userName) {
+    const gmail = google.gmail({ version: 'v1', auth: oAuth2Client });
+    let subject = "Welcome, " + userName + "!";
+    let body = userName + ",\n\n" + 
+                "Thank you for creating an Eventor account!";
+
+    const raw = makeBody(recipientEmail, 'me', subject, body);
+
+    try {
+        console.log("sending email");
+        const response = await gmail.users.messages.send({
+        userId: 'me',
+        resource: { raw },
+        });
+        console.log('Email sent:', response.data.id);
+    } catch (err) {
+        console.error('Error sending email:', err);
+    }
+}
+
 function generateVerificationCode() {
   return ('000000' + (crypto.randomInt(0, 1000000)).toString()).slice(-6);
 }
 
-async function doSendEmail(email, verifyCode) {
+async function doSendVerifyEmail(email, verifyCode) {
   try {
     await new Promise((resolve, reject) => {
       const checkAuth = setInterval(() => {
@@ -168,12 +188,37 @@ async function doSendEmail(email, verifyCode) {
   }
 }
 
+async function doSendConfirmationEmail(email, userName) {
+    try {
+        await new Promise((resolve, reject) => {
+        const checkAuth = setInterval(() => {
+            if (oAuth2Client && oAuth2Client.credentials && oAuth2Client.credentials.access_token) {
+            clearInterval(checkAuth);
+            resolve();
+            }
+        }, 100);
+
+        setTimeout(() => {
+            clearInterval(checkAuth);
+            reject('OAuth2 client not authenticated within the timeout period');
+        }, 60000);
+        });
+
+        await sendConfirmationEmail(email, userName);
+    } catch (error) {
+        console.error('Failed to authenticate OAuth2 client or send email:', error);
+    }
+}
+
 function doVerificationRequest(email) {
   let verifyCode = generateVerificationCode();
-  console.log("- sending verify code: " + verifyCode + " -");
-  doSendEmail(email, verifyCode);
+  doSendVerifyEmail(email, verifyCode);
   
   return verifyCode;
 };
 
-module.exports = {doVerificationRequest};
+function doConfirmationMesssage(email, userName) {
+    doSendConfirmationEmail(email, userName);
+};
+
+module.exports = {doVerificationRequest, doConfirmationMesssage};
