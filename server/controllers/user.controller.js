953e6c80d3daac1eec49aef17a66afa621110bfe
Sean <88566094+izatwin@users.noreply.github.com>
2024-10-10 20:08:15 -0400

    Added password resetting and email verification process (#17)
    Co-authored-by: Vmena9 <vmenapace@icloud.com>

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -1,7 +1,148 @@
 const User = require("../models/user")
+const crypto = require("crypto");
+
+var emailVerifications = {};
+class verificationStatus {
+    constructor(verifyCode) {
+        this.verifyId = crypto.randomBytes(24).toString('hex');
+        this.verifyCode = verifyCode;
+        this.isVerified = false;
+        this.expiration = Date.now() + (10 * 60 * 1000);
+    }
+
+    isExpired() {
+        let expired = (Date.now() > this.expiration);
+        return expired;
+    }
+}
+
+exports.isLoggedIn = async (req, res) => {
+    let req_cookies = req.cookies;
+    if (req_cookies) {
+        let user_id = req_cookies.user_id;
+        if (user_id) {
+            let myUser = await User.findById(user_id);
+            if (myUser) {
+                let auth_token = req_cookies.auth_token;
+                if (auth_token) {
+                    let userCredentials = myUser.userCredentials;
+                    if (userCredentials.matchAuthToken(auth_token)) {
+                        req.send({ "logged-in" : true, "user-info" : myUser.getInfoForClient() });
+                        return;
+                    }
+                }
+            }
+        }
+    }
+    req.send({ "logged-in" : false });
+}
+
+exports.isValidAccountEmail = async (req, res) => {
+    let email = req.body.email;
+    if (!email) {
+        res.status(400).send({
+            message: "Email body field required."
+        });
+    }
+    // TODO sanity checks (type, format)
+    
+    // TODO get user from email?!
+    const myUser = await User.findOne({ "email" : email }).exec();
+    if (myUser) {
+        res.send({ "exists" : true });
+        return;
+    }
+    res.send({ "exists" : false });
+}
+
+exports.beginVerification = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "Request body cannot be empty."
+        });
+        return;
+    }
+
+    let email = req.body.email;
+    if (!email) {
+        res.status(400).send({
+            message: "Email required for verification."
+        });
+        return;
+    }
+
+    // TODO sanity check email
+
+    // let verifyCode = sendVerifyEmail(email); // TODO implement
+    let verifyCode = "000000";
+
+    let myVerificationStatus = new verificationStatus(verifyCode);
+    emailVerifications[email] = myVerificationStatus;
+
+    let verifyId = myVerificationStatus.verifyId;
+    req.send({ "verifyId" : verifyId });
+}
+
+exports.verifyEmail = (req, res) => {
+    if (!req.body) {
+        res.status(400).send({
+            message: "Request body cannot be empty."
+        });
+        return;
+    }
+
+    let email = req.body.email;
+    let verifyId = req.body.verifyId;
+    let verifyCode = req.body.verifyCode;
+    if (!(email && verifyId && verifyCode)) {
+        res.status(400).send({
+            message: "Missing required param(s) for verification."
+        });
+        return;
+    }
+
+    let myVerificationStatus = emailVerifications[email];
+    if (!myVerificationStatus) {
+        res.status(400).send({
+            message: "Verification process not initialized."
+        });
+        return;
+    }
+    if (!(myVerificationStatus.verifyId === verifyId)) {
+        res.status(400).send({
+            message: "Invalid verify Id."
+        });
+        return;
+    }
+    if (myVerificationStatus.isVerified) {
+        res.status(400).send({
+            message: "Already verified!"
+        });
+        return;
+    }
+    if (myVerificationStatus.isExpired()) {
+        res.status(498).send({
+            message: "Verify Id expired, please restart process."
+        });
+        return;
+    }
+
+    let realVerifyCode = myVerificationStatus.verifyCode;
+    if (!(verifyCode === realVerifyCode)) {
+        res.status(401).send({
+            message: "Incorrect verification code."
+        });
+        return;
+    }
+
+    myVerificationStatus.isVerified = true;
+    res.status(200).send({
+        message: "Verified!"
+    });
+}
 
 // Create and save a new User account
-exports.signup = (req, res) => {
+exports.signup = async (req, res) => {
     if (!req.body) {
         res.status(400).send({
             message: "User cannot be empty."
@@ -11,8 +152,10 @@ exports.signup = (req, res) => {
     
     let email = req.body.email;
     let password = req.body.password;
-    console.log("Email: " + email + " - Password: " + password);
-    if (!(email && password)) {
+    let verifyId = req.body.verifyId;
+    let userName = req.body.userName;
+    let displayName = req.body.displayName || userName;
+    if (!(email && password && verifyId && userName)) {
         res.status(400).send({
             message: "Missing param(s) for User creation."
         });
@@ -20,26 +163,77 @@ exports.signup = (req, res) => {
     }
     // TODO sanity checks (type, format)
 
-    // TODO make sure user doesn't already exist in the database with same email, username, or userId
-    console.log("A");
+
+    let myVerificationStatus = emailVerifications[email];
+    if (!myVerificationStatus) {
+        res.status(400).send({
+            message: "Verification process not initialized."
+        });
+        return;
+    }
+    if (!(myVerificationStatus.verifyId === verifyId)) {
+        res.status(400).send({
+            message: "Invalid verify Id."
+        });
+        return;
+    }
+    if (!myVerificationStatus.isVerified) {
+        res.status(400).send({
+            message: "Not yet verified!"
+        });
+        return;
+    }
+    if (myVerificationStatus.isExpired()) {
+        res.status(498).send({
+            message: "Verify Id expired, please restart process."
+        });
+        return;
+    }
+    emailVerifications[email] = null;
+
+    // Make sure user doesn't already exist in the database with same email or username
+    const existingUser = await User.findOne({ "email" : email }).exec();
+    if (existingUser) {
+        res.status(409).send({
+            topic: "email",
+            message: "Email already taken!"
+        });
+        return;
+    }
+    const existingUser2 = await User.findOne({ "userName" : userName }).exec();
+    if (existingUser2) {
+        res.status(409).send({
+            topic: "userName",
+            message: "Username already taken!"
+        });
+        return;
+    }
+
+    // create user
     const newUser = new User({
         email: email,
+        userName : userName,
+        displayName : displayName,
         userCredentials : {}
     });
-    console.log("B");
-    newUser.userCredentials.initCredentials(password);
-    console.log("C");
+
+    let userCredentials = newUser.userCredentials;
+    userCredentials.initCredentials(password)
+
+    let authToken = userCredentials.generateAuthToken();
+
+    res.cookie("user_id", newUser._id)
+    res.cookie("auth_token", authToken.token, {expire: authToken.expire})
+    
     newUser.save(newUser)
         .then(data => {
-            console.log(data);
-            res.send(data);
+            res.status(200).send(data.getInfoForClient());
         })
         .catch(err => {
             res.status(500).json({
                 message: err.message || "server error."
             });
         });
-        console.log("D");
 };
 
 // Log in to an existing account (receive an auth token in the form of a cookie)
@@ -54,7 +248,9 @@ exports.login = async (req, res) => {
                 if (auth_token) {
                     let userCredentials = myUser.userCredentials;
                     if (userCredentials.matchAuthToken(auth_token)) {
-                        res.send(myUser);
+                        res.status(400).send({
+                            message: "Already logged in."
+                        });
                         return;
                     }
                 }
@@ -77,11 +273,11 @@ exports.login = async (req, res) => {
     }
     // TODO sanity checks (type, format)
     
-    // TODO get user from email?!
+    // get user from email
     const myUser = await User.findOne({ "email" : email }).exec();
-    console.log(myUser);
     if (!myUser) {
-        res.status(400).send({
+        res.status(409).send({
+            topic: "email",
             message: "Invalid email!"
         });
         return;
@@ -90,7 +286,8 @@ exports.login = async (req, res) => {
     const userCredentials = myUser.userCredentials;
     
     if (!userCredentials.matchPassword(password)) {
-        res.status(400).send({
+        res.status(409).send({
+            topic: "password",
             message: "Invalid password!"
         });
     }
@@ -103,7 +300,7 @@ exports.login = async (req, res) => {
 
     myUser.save(myUser)
         .then(data => {
-            res.send(data); // TODO don't send EVERYTHING back, especially leave out user credential information
+            res.status(200).send(data.getInfoForClient()); 
         })
         .catch(err => {
             res.status(500).json({
@@ -112,20 +309,129 @@ exports.login = async (req, res) => {
         });
 };
 
-// Find a single user with an id
-function getUserById(userId) {
-    User.findById(userId)
-    .then(data => {
-        if (!data)
-            return res.status(404).send({ message: `User not found with id=${id}` });
-        else res.send(data);
-    })
-    .catch(err => {
-        return res.status(500).send({
-            message: `Error retrieving user with id=${id}`,
-            error: err.message || 'Unexpected Error'
+exports.resetPassword = async (req, res) => {
+    let req_cookies = req.cookies;
+    if (req_cookies) {
+        let user_id = req_cookies.user_id;
+        if (user_id) {
+            let myUser = await User.findById(user_id);
+            if (myUser) {
+                let auth_token = req_cookies.auth_token;
+                if (auth_token) {
+                    let userCredentials = myUser.userCredentials;
+                    if (userCredentials.matchAuthToken(auth_token)) {
+                        res.status(400).send({
+                            message: "Already logged in."
+                        });
+                        return;
+                    }
+                }
+            }
         }
-        );
+    }
+    
+    if (!req.body) {
+        res.status(400).send({
+            message: "User cannot be empty."
+        });
+    }
+    
+    let email = req.body.email;
+    let newPassword = req.body.newPassword;
+    let verifyId = req.body.verifyId;
+    if (!(email && newPassword && verifyId)) {
+        res.status(400).send({
+            message: "Missing param(s) for password reset."
+        });
+    }
+    // TODO sanity checks (type, format)
+    
+    let myVerificationStatus = emailVerifications[email];
+    if (!myVerificationStatus) {
+        res.status(400).send({
+            message: "Verification process not initialized."
+        });
+        return;
+    }
+    if (!(myVerificationStatus.verifyId === verifyId)) {
+        res.status(400).send({
+            message: "Invalid verify Id."
+        });
+        return;
+    }
+    if (!myVerificationStatus.isVerified) {
+        res.status(400).send({
+            message: "Not yet verified!"
+        });
+        return;
+    }
+    if (myVerificationStatus.isExpired()) {
+        res.status(498).send({
+            message: "Verify Id expired, please restart process."
+        });
+        return;
+    }
+    emailVerifications[email] = null;
+
+    // TODO get user from email?!
+    const myUser = await User.findOne({ "email" : email }).exec();
+    if (!myUser) {
+        res.status(400).send({
+            message: "Invalid email!"
+        });
+        return;
+    }
+
+    const userCredentials = myUser.userCredentials;
+    userCredentials.initCredentials(newPassword);
+
+    let authToken = userCredentials.generateAuthToken();
+    myUser.markModified('userCredentials.accessTokens');
+
+    res.cookie("user_id", myUser._id)
+    res.cookie("auth_token", authToken.token, {expire: authToken.expire})
+
+    myUser.save(myUser)
+        .then(data => {
+            res.status(200).send(data.getInfoForClient()); 
+        })
+        .catch(err => {
+            res.status(500).json({
+                message: err.message || "server error."
+            });
+        });
+};
+
+exports.logout = async (req, res) => {
+    let req_cookies = req.cookies;
+    if (req_cookies) {
+        let user_id = req_cookies.user_id;
+        if (user_id) {
+            let myUser = await User.findById(user_id);
+            if (myUser) {
+                let auth_token = req_cookies.auth_token;
+                if (auth_token) {
+                    let userCredentials = myUser.userCredentials;
+                    if (userCredentials.matchAuthToken(auth_token)) {
+                        res.clearCookie("user_id");
+                        res.clearCookie("auth_token");
+                        
+                        myUser.removeAuthToken(auth_token);
+                        myUser.markModified('userCredentials.accessTokens');
+
+                        res.status(200).send({
+                            message: "Logged out!"
+                        });
+
+                        return;
+                    }
+                }
+            }
+        }
+    }
+
+    res.status(400).send({
+        message: "Not logged in!"
     });
 }
 
--- server/models/user.js
+++ server/models/user.js
@@ -94,11 +94,7 @@ UserCredentialsSchema.methods.generateAuthToken = function () {
     let expireTime = nowTime + lifetime;
 
     let accessTokens = this.accessTokens;
-    console.log("Access Tokens: ");
-    console.log(accessTokens);
     accessTokens[token] = expireTime;
-    console.log("Token: " + token);
-    console.log("Match: " + accessTokens[token]);
 
     return {
         'token' : token,
@@ -107,11 +103,7 @@ UserCredentialsSchema.methods.generateAuthToken = function () {
 },
 
 UserCredentialsSchema.methods.matchAuthToken = function (token) {
-    console.log("Looking for token: " + token);
-
     let accessTokens = this.accessTokens;
-    console.log("Access Tokens: ");
-    console.log(accessTokens);
 
     let expireTime = accessTokens[token];
     if (expireTime == null) {
@@ -121,9 +113,12 @@ UserCredentialsSchema.methods.matchAuthToken = function (token) {
 
     let nowTime = Date.now();
 
-    console.log(nowTime <= expireTime);
-
     return (nowTime <= expireTime);
+}
+
+UserCredentialsSchema.methods.removeAuthToken = function (token) {
+    let accessTokens = this.accessTokens;
+    accessTokens[token] = null;
 }
 
 const UserSchema = new Schema({
@@ -149,20 +144,25 @@ const UserSchema = new Schema({
     */
 
     _id: { type: String, required: true, default: crypto.randomUUID },
-    // userName: { type: string, required: true, default: "UNDEF" },
+    userName: { type: String, required: true, default: "UNDEF" },
     email : { type: String, required: true, default: "UNDEF" },
-    // displayName : { type: string, required: true, default: "UNDEF" },
+    displayName : { type: String, required: true, default: "UNDEF" },
 
     userCredentials : UserCredentialsSchema,
     
     // user : {type: User, required: true}
 });
 
-UserSchema.method("toJSON", function() {
-    const { __v, _id, ...object } = this.toObject();
-    object.userId = _id;
-    return object;
-});
+UserSchema.methods.getInfoForClient = function () {
+    let infoForClient = {};
+
+    infoForClient.email = this.email;
+    infoForClient.userId = this._id;
+    infoForClient.userName = this.userName;
+    infoForClient.displayName = this.displayName;
+
+    return infoForClient;
+}
 
 // Virtual for user's URL
 UserSchema.virtual("url").get(function () {
