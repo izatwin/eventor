1bfbcc11e0b18395d927ea2931e5a88481f7fe3f
Sean <88566094+izatwin@users.noreply.github.com>
2024-10-11 05:06:03 -0400

    Create more tests (#27)
    * create-user and delete-user
    
    * deleting a post now deletes post from user
    
    * add all api-tests

--- server/models/post.js
+++ server/models/post.js
@@ -4,11 +4,11 @@ const Schema = mongoose.Schema;
 
 const PostSchema = new Schema({
     timestamp: { type: Date, required: true, default: Date.now },
-    user: {type: String, ref: "User", required: true},
+    user: { type: String, ref: "User", required: true },
     views: { type: Number, min: 0, required: true, default: 0 },
     shares: { type: Number, min: 0, required: true, default: 0 },
     likes: { type: Number, min: 0, required: true, default: 0 },
-    content: { type: String, required: true },
+    content: { type: String },
     is_event: { type: Boolean, required: true },
     // event: {type: Schema.Types.ObjectId, ref: "Event"},
     commentsEnabled: { type: Boolean, required: true, default: true },
