afd29ecef88c916d8277a3b3f034e31d0aaf0e9c
izatwin <88566094+izatwin@users.noreply.github.com>
2024-11-17 15:59:29 -0500

    update feed to use post-react-component

--- frontend/src/pages/Feed.js
+++ frontend/src/pages/Feed.js
@@ -4,30 +4,19 @@ import { useState, useEffect, useRef } from 'react';
 import { useNavigate } from "react-router-dom";
 import axios from 'axios'
 
-import profilePic from './icons/profile.png'; 
-
-import viewIcon from './icons/view.png'
-import likeIcon from './icons/like.png'
-import likedIcon from './icons/liked.png'
-import shareIcon from './icons/share.png'
-import expandIcon from './icons/expand.png'
-import commentIcon from './icons/comment.png'
-
 import { useAuth } from '../AuthContext'; 
-import { usePopup } from '../PopupContext';
+import Post from '../components/Post';
 
 const Feed = () => { 
   const [posts, setPosts] = useState([]);
   const { user, setUser } = useAuth();  
-  const { showSharePopup, updateShareCount, updateLike } = usePopup();
   const [eventsById, setEventsById] = useState({});
+  const [profilesById, setProfilesById] = useState({})
   const observer = useRef(null); 
 
   const navigate = useNavigate();
   
   useEffect(() => {
-    console.log("hiii");
-    console.log("hiii");
     
     axios.get("http://localhost:3001/api/user/validate") 
     .then(response => {
@@ -69,10 +58,19 @@ const Feed = () => {
     const updatedPosts = await Promise.all(
       posts.map(async (post) => {
         try {
-          const response = await axios.get(`http://localhost:3001/api/user/${post.user}`);
+          const postOwner = await axios.get(`http://localhost:3001/api/user/${post.user}`);
           console.log("updatePosts res:")
-          console.log(response)
-          const { displayName, userName, imageURL } = response.data;
+          console.log(postOwner)
+          const { displayName, userName, imageURL } = postOwner.data;
+          console.log(postOwner.data)
+          setProfilesById(prevProfiles => ({
+            ...prevProfiles,
+            [post.user]: {
+              displayName: displayName,
+              userName: userName,
+              pfp: imageURL,
+            }
+          }));
         for (const currentPost of posts) {
           if (currentPost.eventId) {
             if (!eventsById[currentPost.eventId]) {
@@ -143,62 +141,6 @@ useEffect(() => {
   };
 }, [posts.length]);  
 
-
-
-
-
-  const handleShare = async (id) => {
-    const success = await updateShareCount(id);
-    if (success) {
-      setPosts(prevPosts =>
-        prevPosts.map(post =>
-          post._id === id ? { ...post, shares: post.shares + 1 } : post
-        )
-      );
-    } else {
-      console.error('Error updating share count');
-    }
-  };
-
-  const handleLike = async (id) => {
-    var success = false
-    var likedPosts = user.likedPosts || [];
-    if (likedPosts.includes(id)) {
-      // post is already liked, we want to unlike
-      success = await updateLike(id, false);
-      if (success) {
-        setPosts(prevPosts =>
-          prevPosts.map(post =>
-            post._id === id ? { ...post, likes: post.likes - 1 } : post
-          )
-        );
-
-        likedPosts = likedPosts.filter(curId => curId !== id)
-
-      }
-
-    } else {
-      // post is not liked, want to like
-      success = await updateLike(id, true);
-      if (success) {
-        setPosts(prevPosts =>
-          prevPosts.map(post =>
-            post._id === id ? { ...post, likes: post.likes + 1 } : post
-          )
-        );
-        likedPosts.push(id)
-      }
-    }
-    setUser(prevUser => ({
-      ...prevUser,
-      likedPosts: likedPosts
-    }))
-
-    if (!success) {
-      console.error('Error updating like status');
-    }
-  };
-
   return (
     <div className="feed-container">
 
@@ -211,117 +153,15 @@ useEffect(() => {
                 <p> Come back later to see more posts! </p>
               </div>
           ) : (
-            posts.map(post=>{
-              const postEvent = post.eventId && eventsById[post.eventId]
-              const isLiking = user.likedPosts.includes(post._id)
-              return (
-                <div className="post" key={post._id} data-post-id={post._id}>
-                  <div className="post-header"> 
-
-                  <img
-                    src={post.imageURL || profilePic} 
-                    alt="PostProfile" 
-                    className="post-profilepic" 
-                  />
-                  
-                  <div className="post-profile-info">
-                    <div className="post-name">{post.displayName}</div>
-                    <div className="post-username">@{post.userName}</div>
-                  </div>      
-                  
-                  </div>
-
-                  <div className="post-content"> 
-                    {post.content}
-                  </div>
-    
-
-                  {postEvent &&  (
-                    <div className="event"> 
-                      <h1 
-                        className="event-name"> 
-                        {postEvent.eventName} 
-                      </h1>
-                      <p 
-                        className="event-description"> 
-                        {postEvent.eventDescription} 
-                      </p>
-                      {(postEvent.startTime || postEvent.endTime) && (
-                        <div className="event-times"> 
-                          {postEvent.startTime ? new Date(postEvent.startTime).toLocaleString() : ""} 
-                          {postEvent.startTime && postEvent.endTime ? " - " : ""}
-                          {postEvent.endTime ? new Date(postEvent.endTime).toLocaleString() : ""}
-                        </div>
-                      )}                 
-                      {postEvent.embeddedImage && (
-                        <img
-                          src={postEvent.embeddedImage} 
-                          alt="Event Image" 
-                          className="event-embeddedImage" 
-                        />
-                      )}
-                      
-                      {postEvent.type === "NormalEvent" && (
-                        <p className="event-location"> Location: {postEvent.location} </p>
-                      )}
-                      
-                      {postEvent.type === "MusicReleaseEvent" && (
-                        <div>
-                          <h2 className="event-release-title"> <b> {postEvent.releaseTitle} </b> </h2>
-                          <p className="event-release-artist"> {postEvent.releaseArtist} </p>
-                          <p className="event-release-type"> [{postEvent.releaseType}] </p> 
-        
-                          {postEvent.songs.map((song, index) => (
-                              <div className="event-song" key={index}> 
-                                {index + 1}. {song.songTitle} ({song.songArtist}) [{song.songDuration}]
-                              </div>
-                          ))}
-                          <br/>
-                          <i> Apple Music: </i> <a href={postEvent.appleMusicLink} style= {{color: 'black'}}> {postEvent.appleMusicLink} </a>  <br/>
-                          <i> Spotify: </i> <a href={postEvent.spotifyLink} style= {{color: 'black'}} > {postEvent.spotifyLink} </a> <br/> <br/>
-
-    
-                        </div>
-                      )}
-                      {postEvent.type === "TicketedEvent" && (
-                        <div>
-                          <i> Get Tickets: </i> <a href={postEvent.getTicketsLink} style= {{color: 'black'}}> {postEvent.getTicketsLink} </a>  <br/><br/>
-                          {postEvent.destinations.map((destination, index) => (
-                              <div className="event-destination"> 
-                                {index + 1}. {destination.location} ({destination.time})
-                              </div>
-                          ))}
-                          <br/>
-                        </div>
-                      )}
-                      
-                    </div>
-                  )}
-                  
-                  <div className="post-buttons">
-
-                  <img src={viewIcon} alt="View" className="view-icon post-icon"/> 
-                  <div className="views-num num">{post.views}</div>
-                  <img onClick={() => { handleLike(post._id) }} src={isLiking ? likedIcon : likeIcon} alt="Like" className="like-icon post-icon"/> 
-                  <div className="likes-num num"> {post.likes} </div>
-                  <img onClick={()=>{showSharePopup(post._id); handleShare(post._id)}} src={shareIcon} alt="Share" className="share-icon post-icon"/> 
-                  <div className="shares-num num"> {post.shares} </div>
-                  
-                  <div className="expand-comment">
-                    <img src={commentIcon} alt="Comment" className="comment-icon post-icon"/> 
-                    <div className="comment-num num">{post.comments.length}</div>
-                    <img 
-                      src={expandIcon} 
-                      alt="Expand" 
-                      className="expand-icon post-icon"
-                      onClick={()=>{navigate(`/post/${post._id}`)}}/> 
-                  </div>
-
-                  </div>
-
-                </div>
-              )
-            })
+            posts.map((post) => (
+              <Post
+                key={post._id}
+                post={post}
+                poster={profilesById[post.user]}
+                postEvent={post.eventId && eventsById[post.eventId]}
+                setPost={setPosts}
+              />
+            ))
           )}
         </div>
     </div>
