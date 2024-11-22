ef658787f1de32976d7a9826f49d6590a8dcf38d
Vmena9 <vmenapace@icloud.com>
2024-11-21 21:24:12 -0500

    Fixed (again) profanity filtering for events

--- server/controllers/event.controller.js
+++ server/controllers/event.controller.js
@@ -84,8 +84,8 @@ exports.createEvent = async (req, res) => {
         }
 
         // Check for profanity
-        for (field in req.body) {
-            let val = req.body[field]
+        for (field in eventData) {
+            let val = eventData[field]
             if (textfilter.containsProfanity(val)) {
                 res.status(422).json({ message: "Event content contains profanity." });
                 return;
@@ -148,8 +148,8 @@ exports.editEvent = async (req, res) => {
         }
 
         // Check for profanity
-        for (field in req.body) {
-            let val = req.body[field]
+        for (field in eventData) {
+            let val = eventData[field]
             if (textfilter.containsProfanity(val)) {
                 res.status(422).json({ message: "Event content contains profanity." });
                 return;
